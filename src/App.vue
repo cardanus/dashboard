66ccd2d9021c4aefd231f6b74267ef23538db186
Annie Aasen <aasenannie@gmail.com>
2018-08-17 16:21:55 +0300

    Style header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Islandsbanki logo" src="../public/ISB-logo.svg">
+    <div class="row">
+      <div id="title" class="col">
+        <h1>Cardanus Dashboard</h1>
+      </div>
+      <div class="col">
+        <img id="logo" alt="Islandsbanki logo" src="../public/ISB-logo.svg" class="float-right m-4">
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  #title {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+
+  #logo {
+    width: 100px;
+  }
 </style>
