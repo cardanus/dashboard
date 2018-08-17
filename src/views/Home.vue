733ceb24a09b67aa44b1e617bf54601bda4ab5ec
Annie Aasen <aasenannie@gmail.com>
2018-08-17 15:27:58 +0300

    Add a list view of sample data of experiments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home mt-4">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>Experiment ID</th>
+          <th>Status</th>
+          <th>Allocation Rate</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in store.data" :key="item.expId">
+          <td>{{ item.expId }}</td>
+          <td>{{ item.status }}</td>
+          <td>{{ item.allocRate * 10 }}%</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,10 +23,17 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+import { store } from '@/main'
+
 export default {
   name: 'home',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      store,
+    };
+  },
 }
 </script>
